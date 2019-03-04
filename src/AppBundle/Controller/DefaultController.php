695939cbee8f57a5cc626a4342ad5433a01b76ab
dmp03david <34797992+dmp03david@users.noreply.github.com>
2019-03-04 04:25:04 +0100

    Fosuserbundle
    Mise en place fosuserbundle et mise a jour de twig.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,15 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/salarie", name="salarie")
+     */
+    public function salariePage(Request $request)
+    {
+        
+        return $this->render('salarie/salarie.html.twig', [
+            
+        ]);
+    }
 }
